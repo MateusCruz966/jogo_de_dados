21c42bba5d39326c1a7f03a8fa2debfd9bf9afb5
mazao <mazao@MFC>
2025-04-16 17:58:54 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace jogo_de_dados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
